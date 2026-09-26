7305dc7819c16169632f07b5a14d6af18f373941
Julius Vincent Babies <julius@familie-babies.de>
2026-09-26 20:25:40 +0200

    feat(app/settings #108): New LoginDialog component + https prefix

---
+++ app/shared/src/commonMain/kotlin/es/jvbabi/trails/page/setings/login/LoginDialog.kt
@@ -0,0 +1,51 @@
+package es.jvbabi.trails.page.setings.login
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import es.jvbabi.trails.utils.HttpsVisualTransformation
+import org.jetbrains.compose.resources.stringResource
+import trails.app.shared.generated.resources.Res
+import trails.app.shared.generated.resources.common_ok
+import trails.app.shared.generated.resources.settings_login
+import trails.app.shared.generated.resources.settings_login_homeserver_label
+
+@Composable
+fun LoginDialog(
+    url: String,
+    onDismiss: () -> Unit,
+    onSubmit: () -> Unit,
+    onValueChange: (to: String) -> Unit,
+) {
+    AlertDialog(
+        onDismissRequest = onDismiss,
+        title = { Text(stringResource(Res.string.settings_login)) },
+        text = {
+            Column {
+                TextField(
+                    visualTransformation = HttpsVisualTransformation(MaterialTheme.colorScheme.outline),
+                    value = url,
+                    onValueChange = { onValueChange(it) },
+                    label = { Text(stringResource(Res.string.settings_login_homeserver_label)) }
+                )
+            }
+        },
+        confirmButton = {
+            Button(onClick = onSubmit) {
+                Text(stringResource(Res.string.common_ok))
+            }
+        }
+    )
+}
+
+@Preview
+@Composable
+private fun LoginDialogPreview() {
+    LoginDialog(
+        url = "trails.example.com",
+        onDismiss = {},
+        onSubmit = {},
+        onValueChange = {},
+    )
+}
